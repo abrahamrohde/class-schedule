f8c57504a61e785669aeaf6ea3d30475cfc44aa2
Abraham Rohde <abrahamrohde@abrahams-macbook-pro.local>
2015-10-27 17:38:13 -0500

    Initial Commit

---
+++ ClassScheduleTests/ClassScheduleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ClassScheduleTests.swift
+//  ClassScheduleTests
+//
+//  Created by Abraham Rohde on 10/27/15.
+//  Copyright © 2015 Abraham Rohde. All rights reserved.
+//
+
+import XCTest
+@testable import ClassSchedule
+
+class ClassScheduleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
